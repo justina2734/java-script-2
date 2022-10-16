d77d8cdce53b4869db76498f5f46872c6b367f35
justina2734 <justina2734@gmail.com>
2022-10-16 15:21:06 +0900

    비교, 결과 부분 줄바꿈 수정

--- day21/app.js
+++ day21/app.js
@@ -1,24 +1,24 @@
 const game=document.querySelector(".game");
 const numberRange=document.querySelector("h3 .number-range");
-const chooseNumber=document.querySelector("h5 .choose-number");
+const chooseNumber=document.querySelector("h4 .choose-number");
 
 function gameStart(event){
     event.preventDefault();
     const number = Math.round(Math.random()*numberRange.value);
     if(String(chooseNumber.value)=== String(number)){
-        const span = document.createElement("span");
-        span.innerText=`You choose : ${chooseNumber.value}, the machine choose : ${number}`;
-        const result=document.createElement("result");
+        const compare = document.createElement("div");
+        compare.innerText=`You choose : ${chooseNumber.value}, the machine choose : ${number}`;
+        const result=document.createElement("div");
         result.innerText=`You won!`;
-        span.appendChild(result);
-        document.body.appendChild(span);
+        compare.appendChild(result);
+        document.body.appendChild(compare);
     } else {
-        const span = document.createElement("span");
-        span.innerText = `You choose : ${chooseNumber.value}, the machine choose : ${number}`;
-        const result = document.createElement("result");
+        const compare = document.createElement("div");
+        compare.innerText = `You choose : ${chooseNumber.value}, the machine choose : ${number}`;
+        const result = document.createElement("div");
         result.innerText = `You lost!`;
-        span.appendChild(result);
-        document.body.appendChild(span);
+        compare.appendChild(result);
+        document.body.appendChild(compare);
     }
 }
 game.addEventListener("submit", gameStart);
