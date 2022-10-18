22432328535ca6234de0d518aff2fe404b822022
justina2734 <justina2734@gmail.com>
2022-10-18 21:07:40 +0900

    deleting toDos

--- js/todo.js
+++ js/todo.js
@@ -13,12 +13,15 @@ function saveToDos(){
 function deleteTodo(event){
     const li=event.target.parentElement;
     li.remove();
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintTodo(newTodo){
      const li = document.createElement("li");
+     li.id = newTodo.id;
      const span = document.createElement("span");
-     span.innerText = newTodo;
+     span.innerText = newTodo.text;
      const button = document.createElement("button");
      button.innerText="⭕️";
      button.addEventListener("click", deleteTodo);
@@ -31,8 +34,12 @@ function handleTodoSubmit(event){
       event.preventDefault();
       const newTodo = todoInput.value;
      todoInput.value="";
-     toDos.push(newTodo);
-     paintTodo(newTodo);
+     const newTodoObj={
+        text:newTodo,
+        id:Date.now(),
+     }
+     toDos.push(newTodoObj);
+     paintTodo(newTodoObj);
      saveToDos();
 }
 
